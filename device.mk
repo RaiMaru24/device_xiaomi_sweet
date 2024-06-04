b1b8379a78d76370911919678af3ad0c8a27c7a5
Alen 24 <alenbijuk2003@gmail.com>
2024-06-04 08:52:17 +0530

    sweet: include JamesDSP

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
 
+# CAll the JamesDSP
+$(call inherit-product-if-exists, vendor/JamesDSP/config.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
