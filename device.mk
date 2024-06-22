79af98574fc4e841d5222ab3497091c9ec1d6d25
Sonal Singh <sonal.singh.19993@gmail.com>
2024-06-22 09:19:04 +0000

    Build libminijail and libavservices_minijail
    Signed-off-by: Alen24 <alenbijuk2003@gmail.com>

--- device.mk
+++ device.mk
@@ -333,7 +333,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
-    
+ 
+# Minijail
+PRODUCT_PACKAGES += \
+    libminijail \
+    libminijail.vendor \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
