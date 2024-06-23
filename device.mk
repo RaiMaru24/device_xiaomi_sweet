4ca239313a4bd21d0c0229baa6f22eabe05babea
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-23 12:14:17 +0530

    sweet: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352
    Signed-off-by: RaiMaru24 <alenbijuk2003@gmail.com>

--- device.mk
+++ device.mk
@@ -293,6 +293,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
