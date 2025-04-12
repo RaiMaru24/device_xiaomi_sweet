8440c8f8c3751d9c477b0096b3b992eaebfe826d
Karan Parashar <karan@pixelos.net>
2025-04-12 19:00:27 +0530

    sweet: Build libutils.vendor E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library libutils.so not found.
    Signed-off-by: RaiMaru24 <alenbijuk2003@gmail.com>

--- device.mk
+++ device.mk
@@ -303,7 +303,8 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
-    libstagefrighthw
+    libstagefrighthw \
+    libutils.vendor
 
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
